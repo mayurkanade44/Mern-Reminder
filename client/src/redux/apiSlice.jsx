ad9b8252e69e2bcfd5b3e61909e7ea32193c4afa
mayurkanade44 <mayurkanade44@gmail.com>
2023-12-27 11:40:37 +0530

    unauthorized logout fix

--- client/src/redux/apiSlice.jsx
+++ client/src/redux/apiSlice.jsx
@@ -1,9 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { toast } from "react-toastify";
+import { logoutUser } from "./authSlice";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl: "/" });
+const authBaseQuery = async (args, api, option) => {
+  const res = await baseQuery(args, api, option);
+  if (res.error && res.error.status === 401) {
+    toast.error("Unauthorized!! logged out");
+    api.dispatch(logoutUser());
+    return;
+  }
+  return res;
+};
 
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: authBaseQuery,
   tagTypes: ["User", "Reminder"],
   endpoints: (builder) => ({}),
 });
